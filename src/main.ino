7f05a3b8ba1db38091604bf43306206b76ae59e2
RestucciaMichele <72453178+RestucciaMichele@users.noreply.github.com>
2024-06-03 00:22:28 +0200

    aggiunta cella di carico e stepper

--- src/main.ino
+++ src/main.ino
@@ -1,35 +1,70 @@
 #include <Arduino.h>
 #include <TaskScheduler.h>
+#include <Stepper.h>
+#include "HX711.h"
 
 #define _TASK_SLEEP_ON_IDLE_RUN
+#define _TASK_STATUS_REQUEST
 
 // ======= PIN =======
-const int rgbPins[] = {9, 10, 11}; // red, green, blue
+const int echoPin = 2;    // Echo pin for ultrasonic sensor
+const int triggerPin = 3;  // Trigger pin for ultrasonic sensor
+const int LOADCELL_SCK_PIN = 4;
+const int LOADCELL_DOUT_PIN = 5;  
+const int rgbPins[] = {6, 7, 8}; // red, green, blue
+const int motorPin1 = 9;  // IN1 on the ULN2003 driver
+const int motorPin2 = 10;  // IN3 on the ULN2003 driver
+const int motorPin3 = 11;  // IN2 on the ULN2003 driver
+const int motorPin4 = 12;  // IN4 on the ULN2003 driver
 const int buttonPin = 13;
-const int triggerPin = 14;
-const int echoPin = 15;
 
 // ======= VARIABILI GLOBALI =======
-const int contenitoreVuoto = 10; // cm
+const int contenitoreVuoto = 17; // cm
+float distanzaDalCibo;
 
-// ======= MISURAZIONE DISTANZA ULTRASUONI =======
-// non è un task perché viene fatta una chiamata alla volta
+const int giriVite = 20;  // 20 per un giro completo
+
+const int pesoCiboMinimo = 15;
+const int pesoCiboMassimo = 60;
+const float calibration_factor = 2076.89;
+float pesoAttuale;
+
+// ======= MOTORE STEPPER =======
+const int stepsPerRevolution = 2048;  // 1 giro = 2048 passi
+Stepper stepper = Stepper(stepsPerRevolution, motorPin1, motorPin3, motorPin2, motorPin4);
+void stepperMove(int step) {
+  stepper.setSpeed(14);
+  stepper.step(step);
+}
+
+void moveForward() {
+  stepperMove(200);
+  stepperMove(-100);
+}
+
+// ======= BILANCIA, LOAD CELL =======
+HX711 scale;
+void misuraPeso() {
+  scale.power_up();
+  
+  scale.set_scale(calibration_factor);
+  pesoAttuale = scale.get_units(10);
+
+  scale.power_down();
+}
+
+// ======= DISTANZA ULTRASUONI =======
 long durata;
-float distanza;
 void misurazioneDistanza() {
-  digitalWrite(triggerPin, LOW);  // gli impulsi LOW servono per pulire la ricezione del vero impulso HIGH di 10 microsecondi
-  delayMicroseconds(5);
+  digitalWrite(triggerPin, LOW);  // gli impulsi LOW servono per pulire il segnale per la ricezione del vero impulso HIGH di 10 microsecondi
+  delayMicroseconds(5);           // delay obbligatori per il corretto funzionamento del sensore
   digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
 
   durata = pulseIn(echoPin, HIGH);
   
-  distanza = (durata/2.0 * 0.0343); // cm
-  Serial.print("misurazioneDistanza: ");
-  Serial.print(distanza);
-  Serial.print("cm");
-  Serial.println();
+  distanzaDalCibo = (durata/2.0 * 0.0343); // cm
 }
 
 // ================= FADE LED RGB =================
@@ -44,10 +79,8 @@ int fadeAmount = 5;
 void ledFade(String Color) {
   if (Color == "red") {
     setColor(brightness, 0, 0);
-  } else if (Color == "green") {
-    setColor(0, brightness, 0);
-  } else if (Color == "yellow") {
-    setColor(brightness, brightness, 0);
+  } else if (Color == "blue") {
+    setColor(0, 0, brightness);
   } else {
     setColor(0,0,0);
   }
@@ -61,62 +94,75 @@ void ledFade(String Color) {
 
 // ================== TASK SETUP ==================
 void aperturaCoperchio();
-void controlloContenitore();
-void ledFadeRed() {
-  ledFade("red");
-}
-void ledFadeYellow() {
-  ledFade("yellow");
-}
+void controlloCiotola();
+void ledFadeRed() {ledFade("red");}
+void ledFadeBlue() {ledFade("blue");}
+void Motore();
 Scheduler runner;
 Task TaskLedRed(30*TASK_MILLISECOND, TASK_FOREVER, &ledFadeRed);
-Task TaskLedYellow(30*TASK_MILLISECOND, TASK_FOREVER, &ledFadeYellow);
+Task TaskLedBlue(30*TASK_MILLISECOND, TASK_FOREVER, &ledFadeBlue);
 Task TaskAperturaCoperchio(1000*TASK_MILLISECOND, TASK_FOREVER, &aperturaCoperchio);
-Task TaskControlloContenitore(15*TASK_SECOND, TASK_FOREVER, &controlloContenitore);
-
-// ================== CONTROLLO CONTENITORE ==================
-void controlloContenitore() {
-  Serial.print("controlloContenitore ");
-  Serial.println(millis());
-
-  misurazioneDistanza();
-  if (distanza >= contenitoreVuoto) {
-    Serial.println("contenitore vuoto");
-    TaskLedYellow.enable();
-  } else {
-    TaskLedYellow.disable();
-  }
-}
+Task TaskAttivazioneMotore(TASK_MILLISECOND, giriVite, &Motore);
+Task TaskControlloCiotola(40*TASK_SECOND, TASK_FOREVER, &controlloCiotola);
 
 // ================== APERTURA COPERCHIO ==================
-void resetLed() {
-  if (!TaskLedRed.isEnabled() && !TaskLedYellow.isEnabled()) {
-    Serial.print("IF resetLed red :");
-    Serial.print(TaskLedRed.isEnabled());
-    Serial.print(" yellow :");
-    Serial.println(TaskLedYellow.isEnabled());
-    // reset ledFade
-    // in questo modo non rimane accesso dall'ultima chiamata di ledFade
+void resetLed() { // in questo modo non rimane accesso dall'ultima chiamata di ledFade
+  if (!TaskLedRed.isEnabled() && !TaskLedBlue.isEnabled()) {
     brightness = 0;
     fadeAmount = 5;
     ledFade("red"); // va bene qualsiasi colore
-  } else {
-    Serial.print("ELSE resetLed red :");
-    Serial.print(TaskLedRed.isEnabled());
-    Serial.print(" yellow :");
-    Serial.println(TaskLedYellow.isEnabled());
   }
 }
 
 void aperturaCoperchio() {
   if (digitalRead(buttonPin)==LOW) {  // coperchio aperto -> disabilito tutti i task
-    TaskControlloContenitore.disable();
-    TaskLedYellow.disable();
+    Serial.println("COPERCHIO APERTO. Disabilitazione task ...");
+    TaskAttivazioneMotore.disable();
+    TaskControlloCiotola.disable();
+    TaskLedBlue.disable();
     TaskLedRed.enableIfNot();
-  } else {  // coperchio chiuso -> disabilito task led e abilito i gli altri task
+  } else {            // coperchio chiuso -> disabilito task led e abilito i gli altri task
     TaskLedRed.disable();
-    TaskControlloContenitore.enableIfNot();
-    resetLed();
+    resetLed();       //controllo funziona per tutte le chiamate di led (anche per colori diversi), quindi basta questo
+    TaskControlloCiotola.enableIfNot();
+  }
+}
+
+// ================== CONTROLLO BILANCIA E MOTORE CIOTOLA==================
+void Motore() {
+  setColor(0,255,0);
+  moveForward();  
+  setColor(0,0,0);
+}
+
+bool ricaricato = false;
+void controlloCiotola() {
+  Serial.print("controllo quantità scorta di cibo ==> ");
+  misurazioneDistanza();
+  Serial.println(distanzaDalCibo);
+  if (distanzaDalCibo >= contenitoreVuoto) {
+    Serial.println("SCORTA INSUFFICIENTE. ricarica di cibo non possibile ...");
+    TaskLedBlue.enableIfNot(); 
+    return;
+  } else {
+    TaskLedBlue.disable();
+    
+    Serial.print("SCORTA SUFFICIENTE. controllo quantità cibo nella ciotola ==> ");
+    misuraPeso();
+    Serial.println(pesoAttuale);
+    if (pesoAttuale < pesoCiboMinimo || ricaricato == true) {
+      Serial.println("CIBO INSUFFICIENTE. Attivazione motore ...");
+      TaskAttivazioneMotore.restart();
+      ricaricato = true;
+      if (pesoAttuale >= pesoCiboMassimo) {
+        Serial.println("CIBO SUFFICIENTE. Motore non attivato");
+        ricaricato = false;
+        TaskAttivazioneMotore.disable();
+        return;
+      } 
+    } else {
+      Serial.println("CIBO SUFFICIENTE. Motore non attivato");
+    }
   }
 }
 
@@ -125,11 +171,33 @@ void schedulerSetup() {
   runner.init();
 
   runner.addTask(TaskLedRed);
-  runner.addTask(TaskLedYellow);
+  runner.addTask(TaskLedBlue);
   runner.addTask(TaskAperturaCoperchio);
-  runner.addTask(TaskControlloContenitore);
+  runner.addTask(TaskAttivazioneMotore);
+  runner.addTask(TaskControlloCiotola);
   
-  TaskAperturaCoperchio.enable();
+  TaskControlloCiotola.enable();
+  TaskAperturaCoperchio.enable(); 
+}
+
+void loadCellSetup() {
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale();
+  scale.tare();
+  scale.power_down();
+}
+
+void initialSetup() {
+  Serial.println("Inizializzazione dispositivo FelineFoodHub ...");
+  delay(2000);
+  Serial.println("appoggiare sopra la bilancia una ciotola vuota per la calibrazione e taratura ...");
+  delay(2000); Serial.println("...");
+  delay(2000); Serial.println("...");
+  delay(2000); Serial.println("...");
+  Serial.println("Inizio Calibrazione ...");
+  loadCellSetup();
+  Serial.println("Calibrazione completata. Bilancia pronta per l'uso ...");
+  schedulerSetup();
 }
 
 void setup() {
@@ -142,7 +210,7 @@ void setup() {
   pinMode(triggerPin, OUTPUT);  //ultrasonic
   pinMode(echoPin, INPUT);      //ultrasonic
 
-  schedulerSetup();
+  initialSetup();
 }
 
 void loop() {
